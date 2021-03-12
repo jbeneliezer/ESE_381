377d22202415b766d66dd2ae6a1aa83f5cc6bda6
Judah Ben-Eliezer <judahbeneliezer@gmail.com>
2021-03-12 17:14:48 -0500

    added lab5

--- lab5/clk_main_32768Hz/clk_main_32768Hz/main.c
+++ lab5/clk_main_32768Hz/clk_main_32768Hz/main.c
@@ -14,16 +14,10 @@ int main(void)
 	PORTA.DIRSET = PIN7_bm;														// enable output on pin 7.
 
 	CPU_CCP = CCP_IOREG_gc;
-	CLKCTRL.MCLKCTRLA = CLKCTRL_CLKOUT_bm | CLKCTRL_CLKSEL_OSC32K_gc;			// main clock enabled on CLKOUT, main clock set to internal 32.768 kHz oscillator.
-	CPU_CCP = CCP_IOREG_gc;
 	CLKCTRL.MCLKCTRLB = CLKCTRL_PDIV_disable;									// prescalar division disabled.
 	CPU_CCP = CCP_IOREG_gc;
-	CLKCTRL.MCLKCTRLC = CLKCTRL_CFDSRC_CLKMAIN_gc | CLKCTRL_CFDEN_bm;			// clock failure source set to main clock, clock failure detection disabled.
-	CPU_CCP = CCP_IOREG_gc;
-	CLKCTRL.MCLKINTCTRL = CLKCTRL_INTTYPE_INT_gc;								// regular interrupt type.
-	CPU_CCP = CCP_IOREG_gc;
-	CLKCTRL.OSCHFCTRLA = CLKCTRL_RUNSTBY_bm | CLKCTRL_FREQSEL_4M_gc | CLKCTRL_AUTOTUNE_bm;				// run on standby enabled, autotune enabled.
-
+	CLKCTRL.MCLKCTRLA = CLKCTRL_CLKOUT_bm | CLKCTRL_CLKSEL_OSC32K_gc;			// main clock enabled on CLKOUT, main clock set to internal 32.768 kHz oscillator.
+	
     while (1) 
     {
     }
--- lab5/one_MHz/one_MHz/main.c
+++ lab5/one_MHz/one_MHz/main.c
@@ -13,8 +13,6 @@ int main(void)
 {
 	PORTA_DIRSET = PIN7_bm;														// CLK_OUT on PA7.
 	CPU_CCP = CCP_IOREG_gc;
-	CLKCTRL.MCLKCTRLA = CLKCTRL_CLKOUT_bm | CLKSEL_OSCHF_gc;					// main clock enabled on CLKOUT, main clock set to internal high frequency oscillator.
-	CPU_CCP = CCP_IOREG_gc;
 	CLKCTRL.MCLKCTRLB = CLKCTRL_PDIV_disable;									// prescalar division disabled.
 	CPU_CCP = CCP_IOREG_gc;
 	CLKCTRL.MCLKCTRLC = CLKCTRL_CFDSRC_CLKMAIN_gc | CLKCTRL_CFDEN_bm;			// clock failure source set to main clock, clock failure detection enabled.
@@ -22,6 +20,9 @@ int main(void)
 	CLKCTRL.MCLKINTCTRL = CLKCTRL_INTTYPE_INT_gc;								// regular interrupt type.
 	CPU_CCP = CCP_IOREG_gc;
 	CLKCTRL.OSCHFCTRLA = CLKCTRL_RUNSTBY_bm | CLKCTRL_AUTOTUNE_bm;				// run on standby enabled, autotune enabled.
+	CPU_CCP = CCP_IOREG_gc;
+	CLKCTRL.MCLKCTRLA = CLKCTRL_CLKOUT_bm | CLKSEL_OSCHF_gc;					// main clock enabled on CLKOUT, main clock set to internal high frequency oscillator.
+
 
     while (1) 
     {
