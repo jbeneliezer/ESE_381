eae7ad70fafd4ce23596194727a778b10d1b6153
unknown <judahbeneliezer@gmail.com>
2021-04-08 19:42:37 -0400

    updated lab8

--- lab8/MCP23S17_inout_interrupt_test/MCP23S17_inout_interrupt_test/main.c
+++ lab8/MCP23S17_inout_interrupt_test/MCP23S17_inout_interrupt_test/main.c
@@ -1,8 +1,8 @@
 /*
- * MCP23S17_write_test.c
+ * MCP23S17_inout_interrupt_test.c
  *
- * Created: 4/1/2021 4:11:40 PM
- * Author : Judah Ben-Eliezer
+ * Created: 4/2/2021 4:30:41 PM
+ * Author : Judah Ben-Eliezer 
  */ 
 
 #define F_CPU 4000000UL
@@ -17,34 +17,33 @@
 #define READ_opcode 0x41
 
 #include <avr/io.h>
-#include <util/delay.h>
 
 void MCP23S17_SPI_init();
 void MCP23S17_SPI_write(uint8_t, uint8_t, uint8_t);
 
 int main(void)
-{	
-	PORTA.DIRSET = PIN4_bm | PIN6_bm;
-	PORTA.DIRCLR = PIN5_bm;
-	PORTF.DIRSET = PIN2_bm;
-
+{
+	PORTMUX.SPIROUTEA |= PORTMUX_SPI00_bm;
 	SPI0.CTRLA = SPI_ENABLE_bm | SPI_MASTER_bm | SPI_PRESC_DIV4_gc;
 	SPI0.CTRLB = SPI_SSD_bm;
 	
+	PORTC.DIRSET = PIN0_bm | PIN2_bm;
+	PORTC.DIRCLR = PIN1_bm;
+	PORTF.DIRSET = PIN2_bm;
+
 	uint8_t i = 0xFF;
 
 	MCP23S17_SPI_init();
 	
-    while (1) 
-    {
+	while (1)
+	{
 		if (i == 0xFF) {
-			i = 0x00;	
-		} else {
+			i = 0x00;
+			} else {
 			++i;
 		}
-		MCP23S17_SPI_write(WRITE_opcode, OLATBaddr_b1, ~i);
-		_delay_ms(5000);
-    }
+		MCP23S17_SPI_write(WRITE_opcode, OLATBaddr_b1, i);
+	}
 }
 
 void MCP23S17_SPI_init()
@@ -58,11 +57,11 @@ void MCP23S17_SPI_init()
 void MCP23S17_SPI_write(uint8_t opcode, uint8_t addr, uint8_t data)
 {
 	PORTF.OUT &= ~PIN2_bm;
+	while ((SPI0.INTFLAGS & PIN7_bm) == PIN7_bm) {}
 	SPI0.DATA = opcode;
-	while ((SPI0.INTFLAGS & PIN7_bm) == 0x00) {}
+	while ((SPI0.INTFLAGS & PIN7_bm) == PIN7_bm) {}
 	SPI0.DATA = addr;
-	while ((SPI0.INTFLAGS & PIN7_bm) == 0x00) {}
+	while ((SPI0.INTFLAGS & PIN7_bm) == PIN7_bm) {}
 	SPI0.DATA = data;
-	while ((SPI0.INTFLAGS & PIN7_bm) == 0x00) {}
 	PORTF.OUT |= PIN2_bm;
 }
